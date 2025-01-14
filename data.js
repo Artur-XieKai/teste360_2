4ee7a3bcb223d65c989615b529405d30de0417b2
Artur-XieKai <xksepco1@gmail.com>
2025-01-14 07:24:09 -0300

    Upload-1
    Diversas pastas e arquivos exceto tiles

---
+++ data.js
@@ -0,0 +1,87 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-bedroom",
+      "name": "bedroom",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1536,
+      "initialViewParameters": {
+        "yaw": 0.09105757477145282,
+        "pitch": 0.20984984648230665,
+        "fov": 1.2577612937926688
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.513522318669782,
+          "pitch": -0.04238059542785777,
+          "rotation": 0,
+          "target": "1-living-room"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-living-room",
+      "name": "living-room",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1536,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 3.062644482397328,
+          "pitch": 0.13258467611912295,
+          "rotation": 0,
+          "target": "0-bedroom"
+        }
+      ],
+      "infoHotspots": []
+    }
+  ],
+  "name": "Minha Casa",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": false,
+    "fullscreenButton": true,
+    "viewControlButtons": true
+  }
+};
